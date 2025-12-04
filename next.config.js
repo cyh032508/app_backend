ce440ac51486ee51ca41a6ae76a354040460e24d
timp4x4x4 <tim4x4x4@gmail.com>
2025-12-04 15:55:19 +0800

    fix signup

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
-    },
-    responseLimit: false,
-  },
+
   images: {
     domains: [],
   },
